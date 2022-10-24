07afe89d8a796167c90d7879792ebc4d2e21156e
John Wika Haakseth <john@septima.dk>
2022-10-25 00:05:49 +0200

    Updates

---
+++ precache-manifest.b4b87d426ed67ea0401c2d6ea86ba786.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "e54d369a1d07692c45a8",
+    "url": "/so-ein-gis-ding-6/static/css/main.9999ccfe.chunk.css"
+  },
+  {
+    "revision": "e54d369a1d07692c45a8",
+    "url": "/so-ein-gis-ding-6/static/js/main.e54d369a.chunk.js"
+  },
+  {
+    "revision": "8044e24fa10eb6b26bd4",
+    "url": "/so-ein-gis-ding-6/static/css/1.a79ae3ae.chunk.css"
+  },
+  {
+    "revision": "8044e24fa10eb6b26bd4",
+    "url": "/so-ein-gis-ding-6/static/js/1.8044e24f.chunk.js"
+  },
+  {
+    "revision": "1f8d246ccbec4a12700f",
+    "url": "/so-ein-gis-ding-6/static/js/runtime~main.1f8d246c.js"
+  },
+  {
+    "revision": "61529a5f58345487902d0ef12cf6580e",
+    "url": "/so-ein-gis-ding-6/static/media/thomas-kelley-5YtjgRNTli4-unsplash.61529a5f.jpg"
+  },
+  {
+    "revision": "22d327189257130b8da1a52dd985dacf",
+    "url": "/so-ein-gis-ding-6/static/media/waldemar-brandt-U3Ptj3jafX8-unsplash.22d32718.jpg"
+  },
+  {
+    "revision": "05342aca7f9b296b7d000be15bd0f0aa",
+    "url": "/so-ein-gis-ding-6/static/media/andrew-neel-1-29wyvvLJA-unsplash.05342aca.jpg"
+  },
+  {
+    "revision": "a09933ae9bca3b3afbe4363f14203a41",
+    "url": "/so-ein-gis-ding-6/static/media/ahmad-odeh-ckm1yAe6jhU-unsplash.a09933ae.jpg"
+  },
+  {
+    "revision": "a6e3bbfe160cd4334011440e5dca764c",
+    "url": "/so-ein-gis-ding-6/static/media/fleur-dQf7RZhMOJU-unsplash.a6e3bbfe.jpg"
+  },
+  {
+    "revision": "8c6c282a11063e8efd5a5525ee4101b4",
+    "url": "/so-ein-gis-ding-6/static/media/shaka1.8c6c282a.jpg"
+  },
+  {
+    "revision": "8f94e04276fb32e61ef224d558f6b214",
+    "url": "/so-ein-gis-ding-6/static/media/shaka2.8f94e042.jpg"
+  },
+  {
+    "revision": "8bce3549241e11cfffc6b18824806676",
+    "url": "/so-ein-gis-ding-6/index.html"
+  }
+];
